c504ecf3f32dd1a8a1d8feb4983b0e5e236edb0f
Usama Muhammad <usamamuhammad45678@gmail.com>
2022-07-07 07:32:07 +0500

    functional users

--- src/pages/Users/Users.js
+++ src/pages/Users/Users.js
@@ -4,37 +4,40 @@ import {range} from '../../helpers';
 import Card from '../../Card';
 import Pagination from '../../Pagination';
 import { Form } from 'react-bootstrap';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 
-export default class Users extends Component {
-  state = { 
+export default function Users() {
+ const [data, setData]= useState({ 
     items: [],
     pages: [1,2,3,4,5],
     currentPage: 1
-  }
+  })
 
-  fetchData = (url, pages, currentPage, toPage) => {
+  const fetchData = (url, pages, currentPage, toPage) => {
     fetch(url, pages, currentPage, toPage) 
       .then(result => {
         return result.json();
       })
       .then(d => {
         (pages && currentPage) 
-          ? this.setState({
+          ? setData({
               items: d.results,
               pages,
               currentPage: toPage
           })       
-          : this.setState({
+          : setData({
+            ...data,
               items: d.results
           });
       });
   }
 
-  updatePage = (e) => {
+ const updatePage = (e) => {
     e.preventDefault();
 
-    let {pages, currentPage} = this.state;
+    let {pages, currentPage} = data;
     const toPage = Number(e.target.getAttribute('data-value'));
     const url = `https://randomuser.me/api/?page=${toPage}&seed=qwer&results=6`;
 
@@ -42,15 +45,15 @@ export default class Users extends Component {
       ? pages = range((toPage - 2), (toPage + 2)) 
       : pages = [1,2,3,4,5];
 
-    (currentPage !== toPage) && this.fetchData(url, pages, currentPage, toPage); 
+    (currentPage !== toPage) && fetchData(url, pages, currentPage, toPage); 
   }
 
-  componentDidMount() {
-    this.fetchData("https://randomuser.me/api/?page=1&seed=qwer&results=6");
-  }
+ useEffect(()=>{
+    fetchData("https://randomuser.me/api/?page=1&seed=qwer&results=6");
+ },[])
 
-  render() {
-    const {currentPage, pages, items} = this.state;
+
+    const {currentPage, pages, items} = data;
 
     const RenderCards=(item, index)=>{
       console.log(item.item)
@@ -89,7 +92,7 @@ export default class Users extends Component {
             </Form>
 
           <Pagination
-            updatePage={this.updatePage}
+            updatePage={updatePage}
             currentPage={currentPage}
             pages={pages}
           />          
@@ -109,4 +112,120 @@ export default class Users extends Component {
       </div>
     );
   }
-}
+
+
+
+
+
+// import React, { Component } from 'react';
+// import '../../app.css';
+// import {range} from '../../helpers';
+// import Card from '../../Card';
+// import Pagination from '../../Pagination';
+// import { Form } from 'react-bootstrap';
+
+
+// export default class Users extends Component {
+//   state = { 
+//     items: [],
+//     pages: [1,2,3,4,5],
+//     currentPage: 1
+//   }
+
+//   fetchData = (url, pages, currentPage, toPage) => {
+//     fetch(url, pages, currentPage, toPage) 
+//       .then(result => {
+//         return result.json();
+//       })
+//       .then(d => {
+//         (pages && currentPage) 
+//           ? this.setState({
+//               items: d.results,
+//               pages,
+//               currentPage: toPage
+//           })       
+//           : this.setState({
+//               items: d.results
+//           });
+//       });
+//   }
+
+//   updatePage = (e) => {
+//     e.preventDefault();
+
+//     let {pages, currentPage} = this.state;
+//     const toPage = Number(e.target.getAttribute('data-value'));
+//     const url = `https://randomuser.me/api/?page=${toPage}&seed=qwer&results=6`;
+
+//     (currentPage !== toPage) && (toPage > 4 ) 
+//       ? pages = range((toPage - 2), (toPage + 2)) 
+//       : pages = [1,2,3,4,5];
+
+//     (currentPage !== toPage) && this.fetchData(url, pages, currentPage, toPage); 
+//   }
+
+//   componentDidMount() {
+//     this.fetchData("https://randomuser.me/api/?page=1&seed=qwer&results=6");
+//   }
+
+//   render() {
+//     const {currentPage, pages, items} = this.state;
+
+//     const RenderCards=(item, index)=>{
+//       console.log(item.item)
+//       return(
+       
+//             <Card gender={item.item.gender} picture={item.item.picture} name={item.item.name} location={item.item.location} phone={item.item.phone} email={item.item.email}  user={item.item} index={index} />
+         
+//       )
+//     }
+
+//     const setProfileClicked = (user, index) => {
+//       // let selectedUser = user;
+//       // let selectedUserIndex = index;
+//       // history.push({
+//       //     pathname: '/profile',
+//       //     state: {
+//       //         userInfo: selectedUser,
+//       //         userIndex: selectedUserIndex
+//       //     }
+//       // })
+//   }
+
+//     return (
+//       <div className="app">        
+//         <div className="container">
+//           <h3>ReactJS, Fetch API, Random User Generator, Pagination</h3>
+
+//           <Form className="d-flex">
+//               <Form.Control
+//                 type="text"
+//                 name="search"
+//                 onChange={(e) => {}}
+//                 placeholder="Search Products..."
+//                 className="mr-sm-1 ml-sm-5"
+//               ></Form.Control>
+//             </Form>
+
+//           <Pagination
+//             updatePage={this.updatePage}
+//             currentPage={currentPage}
+//             pages={pages}
+//           />          
+
+//           <div className="row">
+//             {
+//                items.map((item, key) => {
+//                 return (
+//                   <RenderCards item={item} index={key}  />
+//                 )
+//               })
+//             }
+         
+//           </div>
+        
+//         </div>
+//       </div>
+//     );
+//   }
+// }
